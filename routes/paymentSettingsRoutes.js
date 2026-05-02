b58b55fda4e44787949442dc9ab8208c86e6984a
Aidges Infotech <153446364+aidgesinfotech@users.noreply.github.com>
2026-05-02 15:30:47 +0530

    Delete routes directory

--- routes/paymentSettingsRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getPublicSettings, getFullSettings, updateSettings } = require('../controllers/paymentSettingsController');
-
-router.get('/public', getPublicSettings);
-router.get('/admin', getFullSettings);
-router.post('/update', updateSettings);
-
-module.exports = router;
